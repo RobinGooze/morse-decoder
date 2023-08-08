63a60660565a737c23f5f68b95fa9e01d8d0a708
--global <aleksey.cherkas99@gmail.com>
2023-08-08 22:47:51 +0300

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,61 +1,89 @@
-var newVar = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"
-
-
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-   let newStr = expr.split("**********");
-   let letterStr = newStr.map((splitedLetter) => {
- 
-    let letter = splitedLetter.match(/.{0,10}/g);
 
-    letter.pop();
+    let newStr = expr.split("**********");
+    let word = newStr.map(splitedWord => splitedWord.match(/.{1,10}/g));
+    let letters = word.map(
+        element => element.map(
+            splitLetter => splitLetter.match(/.{1,2}/g)));
+    let toMorse = [];
+
+    // delete all 00
+
+    letters.forEach(element => {
+        element.map(secondElem => {
+            for (let i = 0; i < secondElem.length; i++) {
+                if (secondElem[i] === "00") {
+                    secondElem.shift();
+                    i--
+                }
+            }
+        });
+    });
+
+    // turn number into morse and to normal string;
+
+  for(let i = 0; i < letters.length; i++){
     
-    return letter;
-   }
-    );
-//    for(let i = 0; i < newStr.length; i++) {
-//     returnStr.push(MORSE_TABLE.map())
-//    }
+    let morseStr = [];
+    let morse = [];
+    
+    for(let k = 0; k < letters[i].length; k++){
+        for(let n = 0; n < letters[i][k].length; n++){
+            if(letters[i][k][n] === "10") letters[i][k][n] = ".";
+            else letters[i][k][n] = "-";
+        }
+        morse.push(letters[i][k].join(""));
+    }
+    
+    morse.forEach(elem => {
+        morseStr.push(MORSE_TABLE[elem]);
+    })
 
-   return letterStr;
+    toMorse.push(morseStr.join(""));
+  }
+    return toMorse.join(" ");
 }
 
-console.log(decode(newVar));
+
+module.exports = {
+    decode
+}
